f9906451581e9fd442e8b4fd22900bcbf499ae4d
mkjuice <masonk@udel.edu>
2022-04-07 18:57:21 -0400

    test env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { config } from 'dotenv';
 import { createApp } from './utils/createApp';
 import './database'
+import { DISCORD_API_URL } from './utils/constants';
 config();
 
 const PORT = process.env.PORT || 3001;
@@ -9,6 +10,7 @@ async function main(){
     try{
         const app = createApp();
         app.listen(PORT, ()=> console.log(`Running on Port ${PORT}`));
+        console.log(`discord token: ${DISCORD_API_URL}`)
     } catch(err){
         console.log(err)
     }
