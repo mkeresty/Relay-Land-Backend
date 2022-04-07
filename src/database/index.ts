93b9ff43c18bf6cdcfd4dd20d44c7c78b1930815
mkjuice <masonk@udel.edu>
2022-04-07 15:10:49 -0400

    mongo setup

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost/discord_dashboard')
+mongoose.connect('mongodb+srv://mkeresty:wwXuyfLz6Dqk3ZWU@mern.yxg7v.mongodb.net/discord_dashboard?retryWrites=true&w=majority')
 .then(()=> console.log('connected to mongoose'))
 .catch((err)=>console.log(err));
